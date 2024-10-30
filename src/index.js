0f21b6352a83704e240f2a031b0d6fc724cb95b7
Vincent Klimjack <vklimjack@gmail.com>
2024-10-30 19:40:09 +0000

    add image to content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles.css"
+import lavenderImage from "./lavender.jpg"
+
+const content = document.querySelector(".content");
+const image = document.createElement("img");
+
+image.src = lavenderImage;
+content.appendChild(image);
