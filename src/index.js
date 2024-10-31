2d828d2d4a5869414434b84959c88f1b0c0e6f31
Vincent Klimjack <vklimjack@gmail.com>
2024-10-31 10:43:51 +0000

    set background image

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import "./styles.css"
-import lavenderImage from "./lavender.jpg"
-
-const content = document.querySelector(".content");
-const image = document.createElement("img");
-
-image.src = lavenderImage;
-content.appendChild(image);
