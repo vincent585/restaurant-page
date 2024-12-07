462b4b3b373ec31d9700488a9bca36bf59a2c8ea
Vincent Klimjack <vklimjack@gmail.com>
2024-12-07 10:19:32 +0000

    use webpack merge

--- webpack.common.js
+++ webpack.common.js
@@ -2,22 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Production',
+            template: './src/template.html',
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    devtool: 'eval-source-map',
-    devServer: {
-        watchFiles: ["./src/template.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/template.html',
-        }),
-    ],
     module: {
         rules: [
             {
@@ -34,4 +32,4 @@ module.exports = {
             },
         ],
     },
-}
+};
